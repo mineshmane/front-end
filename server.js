0e1dd17e4024f65b3584804a48d751eb9ef4b8b7
mineshmane <mineshmane94@gmail.com>
2021-05-19 17:53:44 +0530

    Added heroku

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static('./dist/angular-app-heroku'));
+app.use(express.static('./dist/frontend'));
 // Start the app by listening on the default
 // Heroku port
 app.get('/*',(req,res)=>
 
-res.sendFile('index.html',{root:'dist/angular-app-heroku/'}))
+res.sendFile('index.html',{root:'dist/frontend/'}))
 app.listen(process.env.PORT || 8080);
