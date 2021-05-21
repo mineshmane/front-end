f5acd4b34b6102ce04f05ceb9126da9f38357f41
mineshmane <mineshmane94@gmail.com>
2021-05-21 13:13:11 +0530

    Heroku update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   // baseUrl:'http://localhost:4000/api/'
-  baseUrl:'http://email-apis.herokuapp.com/api/'
+  baseUrl:'https://email-apis.herokuapp.com/api/'
 
 
 };
